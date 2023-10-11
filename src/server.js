3f3e6e7ca0df494b8c49d7c134f82fcaae0fa017
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-10-11 17:18:15 +0200

    Skapa workflow för auth repo (#19)

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000
 const app = express()
 
 app.get('/api', function (req,res) {
-    res.json({message: "Hello World!"})
+    res.json({message: "Hello World! and render! :)"})
 })
 
 app.listen(port, () => console.log("Server started on port " + port))

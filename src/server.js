8920d62c80cbac019385e446c014e710f0d81aeb
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-10-12 13:47:18 +0200

    Skapa post i auth(#11) co-host Yulia

--- src/server.js
+++ src/server.js
@@ -5,10 +5,9 @@ app = require('./app')
 const mongoose = require('mongoose')
 const port = process.env.PORT || 3000
 
+mongoose.set('strictQuery', false);
 
-
-mongoose
-.connect(process.env.DB_URL)
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     console.log("Mongoose connected! :)");
     app.listen(port, () => console.log("Server started on port " + port))

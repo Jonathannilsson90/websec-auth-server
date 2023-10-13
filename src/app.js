e4f850e794d81cad343139b1ec3632f692a50192
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-10-13 09:22:48 +0200

    cors

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ app.use(express.json())
 app.use(cors())
 
 
-app.get('/api', function (req,res) {
+app.get('/api', cors(),  function (req,res) {
     res.json({message: "Hello World! update utan auto trigger! D:"})
 })
 
 
 
 const userRoutes = require('./routes/userRoutes')
-app.use("/api", userRoutes)
+app.use("/api", cors(), userRoutes)
 
 module.exports = app;

4d353cf3507c4f712e3cb4671df2d57dc5aa01ab
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-10-14 20:28:47 +0200

    Skapa ratelimit för att inte överbelasta servern (#39)

--- src/app.js
+++ src/app.js
@@ -2,17 +2,20 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+const rateLimit = require('./middleware/rateLimiter')
+
 app.use(express.json())
 app.use(cors())
 
 
-app.get('/api', cors(),  function (req,res) {
+app.get('/api', cors(), rateLimit,  function (req,res) {
     res.json({message: "Hello World! update utan auto trigger! D:"})
 })
 
 
 
 const userRoutes = require('./routes/userRoutes')
+
 app.use("/api", cors(), userRoutes)
 
 module.exports = app;
---
+++ src/middleware/rateLimiter.js
@@ -0,0 +1,10 @@
+const {rateLimit} = require('express-rate-limit')
+
+const limiter = rateLimit({
+windowsMs: 60 * 1000,
+max: 2,
+message: "Request limit exceeded. Please try again later."
+
+})
+
+module.exports = limiter

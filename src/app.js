82654489a41fedec16f72014b007c72a708d27e2
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-10-17 14:23:27 +0200

    Etablera session ID som sparas i mongoDB (#42)

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const session = require('express-session')
 
+const MongoStore = require('connect-mongo')
 const rateLimit = require('./middleware/rateLimiter')
+
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({mongoUrl: process.env.DB_URL + 'sessions'}),
+    cookie:{
+        maxAge: 1000 * 60 * 60 * 24
+    }
+
+}))
 
 app.use(express.json())
 app.use(cors())

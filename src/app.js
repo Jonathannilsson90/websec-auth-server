4629338439014569ad5c98ffb5b392ced5e11de9
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-10-20 11:36:17 +0200

    adding .env variables in auth to run locally and in prod(#55)

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(session({
     cookie:{
         maxAge: 1000 * 60 * 60 * 24,
         sameSite: process.env.SAMESITE || 'lax',
-        secure: process.env.COOKIE_SECURE || false,
+        secure: 'auto',
     }
 }))
 

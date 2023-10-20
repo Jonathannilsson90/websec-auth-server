5cc8bfd11868c00aab8d467bc062dca6829e219a
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-10-20 10:17:47 +0200

    Lägg till corsOptions i BE för att säkra upp appen(#54)

--- src/app.js
+++ src/app.js
@@ -1,49 +1,46 @@
 const express = require('express')
-const cors = require('cors')
-const app = express()
 const session = require('express-session')
-
 const MongoStore = require('connect-mongo')
-const rateLimit = require('./middleware/rateLimiter')
+const cors = require('cors')
 const passport = require('passport')
+const app = express()
 
+const rateLimit = require('./middleware/rateLimiter')
 
+const corsOptions = {
+    origin: process.env.URL,
+    credentials: true, 
+  };
 
 const initializePassport = require('./auth/passportConfig')
 initializePassport(passport)
 
-
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use(session({
     secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     store: MongoStore.create({mongoUrl: process.env.DB_URL}),
     cookie:{
-        maxAge: 1000 * 60 * 60 * 24
+        maxAge: 1000 * 60 * 60 * 24,
+
     }
 }))
+
 app.use(passport.initialize())
 app.use(passport.session())
 
 app.set('trust proxy', true)
 
-app.get('/api', cors(), rateLimit, function (req,res) {
-    if(!req.session.views) {
-        req.session.views = 1;
-    }
-
-    const sessionId = req.sessionID
-    const views = req.session.views++
-    res.json({message: "Hello World!", sessionId, views})
+app.get('/api', rateLimit, function (req,res) {
+    
+    res.json({message: "Hello World!"})
 })
 
-
-
 const userRoutes = require('./routes/userRoutes')
+app.use("/api", userRoutes)
 
-app.use("/api", cors(), userRoutes)
 
 module.exports = app;
